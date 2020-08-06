48052dfbbaf49dc53900f04e363a1ba24320b790
JoshuaLowe1002 <joshualowe1002@gmail.com>
2020-08-06 12:45:28 +0000

    Deploying to gh-pages from  @ e7652f2ad4e369d9255fbe6585dc6e4a38bc6487 🚀

---
+++ precache-manifest.a0028b0c2ccfa81c176a3a31a5aaa694.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "840de4d587dd20236472",
+    "url": "/css/app.19ccd16c.css"
+  },
+  {
+    "revision": "840de4d587dd20236472",
+    "url": "/js/app.af27af7d.js"
+  },
+  {
+    "revision": "66f3e7c894bb7a819063",
+    "url": "/css/chunk-vendors.7669458a.css"
+  },
+  {
+    "revision": "66f3e7c894bb7a819063",
+    "url": "/js/chunk-vendors.c4a1b35f.js"
+  },
+  {
+    "revision": "1511c8de2c5ff61c0d5692868cc3c8f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d38a2b58df330c85e0029eecf71d7c26",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
