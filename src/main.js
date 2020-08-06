8632387444be497af41e8478b53d636bd55272b0
Joshua Lowe <joshualowe1002@gmail.com>
2020-08-06 15:42:31 +0100

    Show taskbar on mobile

--- src/main.js
+++ src/main.js
@@ -7,10 +7,10 @@ import './scss/app.scss'
 import './assets/styles/index.css'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faArrowAltCircleRight, faCamera, faBarcode } from '@fortawesome/free-solid-svg-icons'
+import { faArrowAltCircleRight, faCamera, faBarcode, faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faArrowAltCircleRight, faCamera, faBarcode);
+library.add(faArrowAltCircleRight, faCamera, faBarcode, faBars);
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
--- src/views/Taskbar.vue
+++ src/views/Taskbar.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="h-16 w-full bg-white shadow-xl py-3 fixed z-0">
-        <img class="h-10 t-1 float-right" style="margin-right: 15px;" src="../../public/img/fclogo.png">
+        <div class="inline-block text-2xl mx-2" id="menu" @click="showNav()">
+          <font-awesome-icon icon="bars" />
+        </div>
+        <div class="inline-block float-right">
+          <img class="h-10 t-1" style="margin-right: 15px;" src="../../public/img/fclogo.png">
+        </div>
     </div>
 </template>
 
@@ -10,10 +15,17 @@ export default {
     return {
       msg: 'Post vue'
     };
+  },
+  methods: {
+    showNav() {
+      document.getElementById("navbar").style.display = "block";
+    }
   }
 };
 </script>
 
 <style>
- 
+#menu {
+  cursor: pointer;
+}
 </style>
