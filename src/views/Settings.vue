8458d6a6f11604a9f8189cc001d3788bb9961260
Joshua Lowe <joshualowe1002@gmail.com>
2020-08-06 23:04:32 +0100

    About section

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -13,6 +13,12 @@
                     Delete Stored Option
                 </button>
             </div>
+
+            <div class="Right p-6 bg-white shadow-md rounded-lg" id="settings-left">
+                <span class="text-2xl font-extrabold">About:</span>
+                <br>
+                <span class="text-lg font-extrabold">Crescent - Built on Vue</span>
+            </div>
         </div>
     </div>
 </template>
